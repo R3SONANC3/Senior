5d2e4be0ca3c47314238c9462be1a1aea8dfc9b4
R3SONANC3 <jeerapat5870@gmail.com>
2024-05-27 16:07:46 +0700

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // Optional: if you have global styles
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import MainApp from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId='170385751378-3teihqn74b55q3nf4tc6srft4veslo9g.apps.googleusercontent.com'>
-      <App />
+      <MainApp />
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
